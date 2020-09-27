a8ffcce03f35b2599963c4951751a36d82fab700
Rezkynnd <ichimarugin609@yahoo.com>
2020-09-27 17:24:57 +0800

    feat: myCard

---
+++ src/components/Card.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+// import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+// import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function SimpleCard() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Makassar Dev Society
+        </Typography>
+        <Typography variant="h5" component="h2">
+          Rezky Nanda
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Laki-Laki
+        </Typography>
+        <Typography variant="body2" component="p">
+          do what you want to do
+          <br />
+          {'"make smile yourself"'}
+        </Typography>
+      </CardContent>
+      {/* <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+    </Card>
+  );
+}
